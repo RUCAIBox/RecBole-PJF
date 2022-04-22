4751a562fec760b539324ad777523b3a823b6bda
flust <254170321@qq.com>
2022-04-22 14:24:31 +0800

    ADD: neu sampling

--- pjfbole/data/dataloader.py
+++ pjfbole/data/dataloader.py
@@ -8,9 +8,9 @@ pjfbole.data.dataloader
 """
 
 import torch
-
+import numpy as np
 from recbole.data.dataloader.general_dataloader import TrainDataLoader, NegSampleEvalDataLoader
-
+from recbole.data.interaction import Interaction
 
 class DPGNNTrainDataloader(TrainDataLoader):
     def __init__(self, config, dataset, sampler, shuffle=False):
@@ -38,6 +38,9 @@ class DPGNNTrainDataloader(TrainDataLoader):
 class IPJFTrainDataloader(TrainDataLoader):
     def __init__(self, config, dataset, sampler, shuffle=False):
         super().__init__(config, dataset, sampler, shuffle=shuffle)
+        self.user_inter = dataset.user_single_inter[dataset.user_single_inter[dataset.label_field] == 0]
+        self.item_inter = dataset.item_single_inter[dataset.item_single_inter[dataset.label_field] == 0]
+        self.neu_prefix = 'neu_'
 
     def change_direction(self):
         self.uid_field, self.iid_field = self.iid_field, self.uid_field
@@ -53,11 +56,54 @@ class IPJFTrainDataloader(TrainDataLoader):
         inter_feat_neg_u = super(IPJFTrainDataloader, self)._neg_sampling(inter_feat)
         self.change_direction()
 
+        inter_feat_nue = self._neu_sampling(inter_feat)
+
         inter_feat.update(inter_feat_neg_i)
         inter_feat.update(inter_feat_neg_u)
+        inter_feat.update(inter_feat_nue)
         return inter_feat
+
+    def get_neu_ids(self):
+        # get used neu ids for sampling
+        self.used_item_id = [[] for _ in range(self.sampler.user_num)]
+        for uid, iid in zip(self.user_inter[self.uid_field].to_numpy(), self.user_inter[self.iid_field].to_numpy()):
+            self.used_item_id[uid].append(iid)
+        self.used_item_id = np.array(self.used_item_id)
+
+        self.used_user_id = [[] for _ in range(self.sampler.item_num)]
+        for uid, iid in zip(self.item_inter[self.uid_field].to_numpy(), self.item_inter[self.iid_field].to_numpy()):
+            self.used_user_id[iid].append(uid)
+        self.used_user_id = np.array(self.used_user_id)
+
+    def neu_sample_by_ids(self, key_ids, used_ids, num, field):
+        # neu sample by ids
+        neu_num = len(key_ids)
+        value_ids = np.zeros(neu_num, dtype=np.int64)
+        check_list = np.arange(neu_num)
+        value_ids[check_list] = np.random.randint(1, num, len(check_list))
+        value_ids = [value_ids[i] if len(used_ids[key_ids[i]]) == 0
+                     else used_ids[key_ids[i]][value_ids[i] % len(used_ids[key_ids[i]])]
+                     for i in check_list]
+        neu_ids = torch.tensor(value_ids)
+        neu_feat = Interaction({field: neu_ids})
+        neu_feat = self.dataset.join(neu_feat)
+        neu_feat.add_prefix(self.neu_prefix)
+        return neu_feat
+
+    def _neu_sampling(self, inter_feat):
+        self.get_neu_ids()
+        user_ids = inter_feat[self.uid_field].numpy()
+        item_ids = inter_feat[self.iid_field].numpy()
+
+        neu_item_feat = self.neu_sample_by_ids(user_ids, self.used_item_id, self.sampler.item_num, self.iid_field)
+        neu_user_feat = self.neu_sample_by_ids(item_ids, self.used_user_id, self.sampler.user_num, self.uid_field)
+
+        neu_item_feat.update(neu_user_feat)
+        return neu_item_feat
 
     def _next_batch_data(self):
         cur_data = self._neg_sampling(self.dataset[self.pr:self.pr + self.step])
         self.pr += self.step
         return cur_data
+
+
