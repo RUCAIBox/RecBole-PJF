93d815485c6d57adf0657d7370316ffc595555ae
flust <254170321@qq.com>
2022-04-27 10:48:47 +0800

    BUG:DPGNN

--- pjfbole/data/dataset.py
+++ pjfbole/data/dataset.py
@@ -8,6 +8,7 @@ pjfbole.data.pjf_dataset
 """
 from typing import Any
 
+import copy
 import os
 import numpy as np
 import pandas as pd
@@ -53,8 +54,8 @@ class PJFDataset(Dataset):
         if self.config['multi_direction']:
             self.direct_field = self.config['DIRECT_FIELD']
             self.geek_direct = self.field2token_id[self.direct_field]['0']
-            self.user_single_inter = self.inter_feat[self.inter_feat[self.direct_field] == self.geek_direct]
-            self.item_single_inter = self.inter_feat[self.inter_feat[self.direct_field] != self.geek_direct]
+            # self.user_single_inter = self.inter_feat[self.inter_feat[self.direct_field] == self.geek_direct]
+            # self.item_single_inter = self.inter_feat[self.inter_feat[self.direct_field] != self.geek_direct]
 
         self.inter_feat = self.inter_feat[self.inter_feat[self.label_field] == 1]
         if self.config['ADD_BERT'] and self.user_doc is not None:
@@ -316,11 +317,11 @@ class PJFDataset(Dataset):
     def user_single_inter_matrix(self, form='coo', value_field=None):
         if not self.uid_field or not self.iid_field:
             raise ValueError('dataset does not exist uid/iid, thus can not converted to sparse matrix.')
-        # user_single_inter = self.inter_feat[self.inter_feat[self.direct_field] == self.geek_direct]
-        return self._create_sparse_matrix(self.user_single_inter, self.uid_field, self.iid_field, form, value_field)
+        user_single_inter = self.inter_feat[self.inter_feat[self.direct_field] == self.geek_direct]
+        return self._create_sparse_matrix(user_single_inter, self.uid_field, self.iid_field, form, value_field)
 
     def item_single_inter_matrix(self, form='coo', value_field=None):
         if not self.uid_field or not self.iid_field:
             raise ValueError('dataset does not exist uid/iid, thus can not converted to sparse matrix.')
-        # item_single_inter = self.inter_feat[self.inter_feat[self.direct_field] != self.geek_direct]
-        return self._create_sparse_matrix(self.item_single_inter, self.uid_field, self.iid_field, form, value_field)
+        item_single_inter = self.inter_feat[self.inter_feat[self.direct_field] != self.geek_direct]
+        return self._create_sparse_matrix(item_single_inter, self.uid_field, self.iid_field, form, value_field)
