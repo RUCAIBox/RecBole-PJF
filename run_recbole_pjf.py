75691c200337c9f384fdc3448d5ecf727a3ee8af
flust <254170321@qq.com>
2022-05-27 09:24:39 +0800

    0527

--- run_recbole_pjf.py
+++ run_recbole_pjf.py
@@ -11,7 +11,7 @@ from recbole_pjf.quick_start import run_recbole_pjf
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', type=str, default='BPR', help='name of models')
-    parser.add_argument('--dataset', '-d', type=str, default='ml-100', help='name of datasets')
+    parser.add_argument('--dataset', '-d', type=str, default='jobrec', help='name of datasets')
     parser.add_argument('--config_files', type=str, default=None, help='config files')
 
     args, _ = parser.parse_known_args()
