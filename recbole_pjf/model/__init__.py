8008537b9c193dc91cc2e34cb56e7099bd581248
flust <254170321@qq.com>
2022-08-29 15:25:54 +0800

    fix bug

--- recbole_pjf/model/__init__.py
+++ recbole_pjf/model/__init__.py
@@ -1,7 +1,7 @@
 from recbole_pjf.model.pjfnn import PJFNN
 from recbole_pjf.model.apjfnn import APJFNN
 from recbole_pjf.model.bpjfnn import BPJFNN
-from recbole_pjf.model.dpgnn import DPGNN
+# from recbole_pjf.model.dpgnn import DPGNN
 from recbole_pjf.model.bert import BERT
 from recbole_pjf.model.ipjf import IPJF
 from recbole_pjf.model.lfrr import LFRR
